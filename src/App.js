6f29af100be56294d401e6e1b8cae304a40195a9
Nick Neessen <nick.neessen@gmail.com>
2024-02-02 16:14:53 -0600

    flash card project completed

--- src/App.js
+++ src/App.js
@@ -44,13 +44,18 @@ const questions = [
 ];
 
 function FlashCards() {
-  const [selectedId, setSelectedId] = useState(9103);
+  const [selectedId, setSelectedId] = useState(null);
+
+  function handleClick(id) {
+    setSelectedId(id !== selectedId ? id : null);
+  }
 
   return (
     <div className="flashcards">
       {questions.map((question) => (
         <div
           key={question.id}
+          onClick={() => handleClick(question.id)}
           className={question.id === selectedId ? "selected" : ""}
         >
           <p>
